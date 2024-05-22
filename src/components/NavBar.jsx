08cb81bd65d3056a92df01f623f05931880ef91e
NicoGER1 <nico3g@hotmail.fr>
2024-05-22 23:21:34 +0200

    responsive and burger menu done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import { useState, useEffect } from "react";
 import icon from "../assets/logo3.png";
 import "../styles/NavBar.css";
+import burger from "../assets/burger.png";
+import about from "../assets/about.png";
+import contact from "../assets/contact.png";
+import project from "../assets/project.png";
+import home from "../assets/home.png";
 
 const NavBar = () => {
-  const [showNav, setShowNav] = React.useState(false);
+  const [showNav, setShowNav] = useState(false);
+  const [burgerMenu, setBurgerMenu] = useState(false);
 
-  React.useEffect(() => {
+  useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 0) {
         setShowNav(true);
@@ -21,23 +27,39 @@ const NavBar = () => {
     };
   }, []);
 
+  const handleBurgerClick = () => {
+    setBurgerMenu(!burgerMenu);
+  };
+
   return (
     <nav className={`navbar ${showNav ? "navbar-show" : ""}`}>
       <img className="logo" src={icon} alt="logo de NG" />
-      <ul>
+
+      <ul className={burgerMenu ? "show" : ""}>
         <li>
-          <a href="#Home">Accueil</a>
+          <a href="#Home">
+            <img src={home} alt="Accueil" className="picNav" />
+          </a>
         </li>
         <li>
-          <a href="#AboutMe">A propos</a>
+          <a href="#AboutMe">
+            <img src={about} alt="A propos" className="picNav" />
+          </a>
         </li>
         <li>
-          <a href="#Project">Projets</a>
+          <a href="#Project">
+            <img src={project} alt="Projets" className="picNav" />
+          </a>
         </li>
         <li>
-          <a href="#Contact">Contact</a>
+          <a href="#Contact">
+            <img src={contact} alt="Contact" className="picNav" />
+          </a>
         </li>
       </ul>
+      <div className="burger-icon" onClick={handleBurgerClick}>
+        <img src={burger} alt="menu icon" className="burger" />
+      </div>
     </nav>
   );
 };
